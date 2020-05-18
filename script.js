57daaec95ce0038d4c289224a36db7d54ac304d5
Philemon Heng <phil.hsy@outlook.com>
2020-05-18 23:09:53 +0800

    edited files

--- script.js
+++ script.js
@@ -11,12 +11,25 @@ let controlsArr = ['start', 'pause', 'stop', 'reset'];
 for (i of controlsArr) {
     const control = document.createElement('button');
     control.classList.add('control');
-    control.classList.add(i);
-    control.id = i;
-    control.textContent = i;
+    const img = document.createElement('img');
+    img.classList.add(i);
+    img.classList.add('control');
+    if (i == 'start') {
+        img.src = 'icons/start.svg';
+    } else if (i == 'pause') {
+        img.src = 'icons/pause.svg';
+    } else if (i == 'stop') {
+        img.src = 'icons/stop.svg';
+    } else if (i == 'reset') {
+        img.src = 'icons/reset.svg';
+    }
+    control.appendChild(img);
     controlsContainer.appendChild(control);
 }
+
 timerContainer.appendChild(controlsContainer);
+
+
 
 const adjustContainer = document.createElement('div');
 adjustContainer.classList.add('adjust-container');
@@ -29,6 +42,12 @@ for (i of sessionArr) {
     adjustSession.classList.add('adjust-session');
     adjustSession.id = i == '25:00' ? 'twenty-five' : i;
     adjustSession.textContent = i;
+    if (i == 'inc' || i == 'dec') {
+        adjustSession.textContent = '';
+        let img = document.createElement('img');
+        img.src = i == 'inc' ? 'icons/inc.svg' : 'icons/dec.svg';
+        adjustSession.appendChild(img);
+    } 
     adjustSessionContainer.appendChild(adjustSession);
 }
 
@@ -40,6 +59,12 @@ for (i of breakArr) {
     adjustBreak.classList.add('adjust-break');
     adjustBreak.id = i == '5:00' ? 'five' : i;
     adjustBreak.textContent = i;
+    if (i == 'inc' || i == 'dec') {
+        adjustBreak.textContent = '';
+        let img = document.createElement('img');
+        img.src = i == 'inc' ? 'icons/inc.svg' : 'icons/dec.svg';
+        adjustBreak.appendChild(img);
+    } 
     adjustBreakContainer.appendChild(adjustBreak);
 }
 
@@ -48,10 +73,32 @@ adjustContainer.appendChild(display);
 adjustContainer.appendChild(adjustBreakContainer);
 timerContainer.appendChild(adjustContainer);
 
-// bottom chunk
-const bottom = document.createElement('div');
-bottom.classList.add('bottom');
-timerContainer.appendChild(bottom);
-
-
 // function
+const buttons = document.querySelector('.layout-container'); 
+const clickedPlay = buttons.addEventListener('click', clickedPlayEvent);
+const clickedPause = buttons.addEventListener('click', clickedPauseEvent);
+let paused = false;
+
+function clickedPlayEvent(e) {
+    if (e.target.classList.contains('start')) {
+        console.log('start');
+        paused = false;
+        let minutes = 25;
+        let seconds = 0;
+        let theInterval = setInterval(function() { 
+            minutes--;
+            display.textContent = minutes + ':00';
+            console.log('hi');
+            if (minutes == 0 || paused == true) {
+                clearInterval(theInterval);
+            }
+        }, 1000);
+    }
+}
+
+function clickedPauseEvent(e) {
+    if (e.target.classList.contains('pause')) {
+        console.log('pause');
+        paused = true;
+    }
+}
